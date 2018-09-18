4064d57b6d5f05c9f6b0956593c15a31461237a5
Drake Hunt <HUNTR187@hotmail.com>
2018-09-17 21:45:13 -0600

    Started making a platformer game

---
+++ core/src/com/drake/game/Screens/PlayScreen.java
@@ -0,0 +1,68 @@
+package com.drake.game.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.drake.game.Platform2D;
+import com.drake.game.Scenes.Hud;
+
+public class PlayScreen implements Screen {
+
+    private Platform2D game;
+
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
+    private Hud hud;
+
+    public PlayScreen(Platform2D game) {
+        this.game = game;
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(Platform2D.V_WIDTH, Platform2D.V_HEIGHT, gameCam);
+        hud = new Hud(game.batch);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ desktop/src/com/drake/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.drake.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.drake.game.Platform2D;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Platform2D(), config);
+	}
+}
